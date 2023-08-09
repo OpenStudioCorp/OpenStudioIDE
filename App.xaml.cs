4927886b87c46a4464f98eb3ebc52e6f3fdb0ddd
Charlie <gamerboy030607@gmail.com>
2023-08-09 19:07:28 +1000

    update things

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,6 @@ namespace OpenStudioIDE
     /// </summary>
     public partial class App : Application
     {
+        
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,25 +4,52 @@ using System.Windows;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
-using System.Collections.Generic;
+using Microsoft.Build.Locator;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Execution;
 using System.Linq;
 using Microsoft.Win32;
+using System.Diagnostics;
+using System;
+using Microsoft.Build.Exceptions;
+using Microsoft.Build.Framework;
 
 namespace OpenStudioIDE
 {
     public partial class MainWindow : Window
     {
+        private string selectedSolutionFilePath; // Store the selected .sln file path here
+        private string recentProjectsFilePath = "recient.txt";
+
         public MainWindow()
         {
             InitializeComponent();
+            LoadRecentProjects();
+            
         }
 
         private void Open_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Solution files (*.sln)|*.sln"; // Only show .sln files in the file dialog
+
             if (openFileDialog.ShowDialog() == true)
             {
                 string filePath = openFileDialog.FileName;
+                string extension = Path.GetExtension(filePath);
+
+                if (extension.Equals(".sln", StringComparison.OrdinalIgnoreCase))
+                {
+                    // If the selected file is a .sln file, store its path for later use
+                    selectedSolutionFilePath = filePath;
+                }
+                else
+                {
+                    // Handle the case when a non-.sln file is selected (optional)
+                    MessageBox.Show("Please select a .sln file.", "Invalid File", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+
+                // Load the file content into the editor (if needed)
                 string fileContent = File.ReadAllText(filePath);
                 txtEditor.Text = fileContent;
             }
@@ -79,5 +106,67 @@ namespace OpenStudioIDE
                 // Code is error-free, you can provide some feedback to the user.
             }
         }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Build_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedSolutionFilePath))
+            {
+                // Call the BuildSolution method with the stored solution file path
+                BuildSolution(selectedSolutionFilePath);
+            }
+            else
+            {
+                // Handle the case when no .sln file is selected (optional)
+                MessageBox.Show("Please select a .sln file before building.", "No .sln File Selected", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        private void LoadRecentProjects()
+        {
+            if (File.Exists(recentProjectsFilePath))
+            {
+                // Read recent project paths from the text file
+                var recentProjects = File.ReadAllLines(recentProjectsFilePath);
+
+                // Add recent project paths to the ListBox
+                
+            }
+        }
+
+        private void SaveRecentProject(string projectPath)
+        {
+            // Save the recent project path to the text file
+            File.AppendAllText(recentProjectsFilePath, $"{projectPath}{Environment.NewLine}");
+
+            // Reload the recent projects list
+            LoadRecentProjects();
+        }
+
+        private void txtEditor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+        public void BuildSolution(string solutionFilePath)
+        {
+            try
+            {
+                Process.Start($"dotnet run {solutionFilePath}");
+                MessageBox.Show(solutionFilePath);
+            }
+            catch (InvalidProjectFileException ex)
+            {
+                // Handle any exception related to the project file
+                Console.WriteLine($"InvalidProjectFileException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // Handle other exceptions
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
     }
 }
